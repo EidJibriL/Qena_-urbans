b6d62d10e348af08d5489f482ecbf6b5ccb54ad8
EidJibriL <151285729+EidJibriL@users.noreply.github.com>
2023-11-19 03:26:52 +0200

    Add files via upload

---
+++ map.js
@@ -0,0 +1,19 @@
+function map_story(){
+    window.open("https://arcg.is/49DXH",'_blank')
+}
+function dashboard(){
+    window.open("https://arcg.is/ry84b0",'_blank')
+}
+function dashboard1(){
+    window.open("https://arcg.is/1jrW0P",'_blank')
+}
+function dashboard2(){
+    window.open("https://arcg.is/1a5HeD",'_blank')
+}
+function dashboard3(){
+    window.open("https://arcg.is/05iann",'_blank')
+}
+function survey(){
+    window.open("https://arcg.is/05Dz5H",'_blank')
+}
+
